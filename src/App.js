935cbbac9a5b2ae40687a9a7e99383f07b9531b5
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-04-27 15:57:53 +0530

    Used Card Layout instead of Media to give more menu like look

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import React,{Component} from 'react';
 import { Navbar, NavbarBrand, NavItem } from 'reactstrap';
 import './App.css';
 import Menu from './components/menucomponent';
+import {DISHES} from './shared/dishes';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dishes: DISHES
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +20,7 @@ export default class App extends Component {
               <NavbarBrand href="/">BaIG RESTAURANT</NavbarBrand>
             </div>
           </Navbar>
-          <Menu />
+          <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
