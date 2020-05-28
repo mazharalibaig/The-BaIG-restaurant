a8e0a89f1d0b572befdb0419e2693e9ca4cc7381
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-05-28 16:09:15 +0530

    Updates

---
+++ precache-manifest.e7c4bf3858dd41fdab26027f9ff8c0f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e146325f5c1be0d8fec4d3a1f93841c0",
+    "url": "/The-BaIG-restaurant/index.html"
+  },
+  {
+    "revision": "73e358c79ba1912d10e9",
+    "url": "/The-BaIG-restaurant/static/css/2.4b82c098.chunk.css"
+  },
+  {
+    "revision": "d2953f43709adcaf77d7",
+    "url": "/The-BaIG-restaurant/static/css/main.e1033cc7.chunk.css"
+  },
+  {
+    "revision": "73e358c79ba1912d10e9",
+    "url": "/The-BaIG-restaurant/static/js/2.72affe92.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/The-BaIG-restaurant/static/js/2.72affe92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2953f43709adcaf77d7",
+    "url": "/The-BaIG-restaurant/static/js/main.bcac3939.chunk.js"
+  },
+  {
+    "revision": "d92de866348fdef45a5e",
+    "url": "/The-BaIG-restaurant/static/js/runtime-main.e18657cb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/The-BaIG-restaurant/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/The-BaIG-restaurant/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/The-BaIG-restaurant/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/The-BaIG-restaurant/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/The-BaIG-restaurant/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
